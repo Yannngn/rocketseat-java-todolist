6dac8536f502b1ef5b3f1a6459213a9bdfc50a38
yannngn <yann.nicholas@gmail.com>
2023-10-11 01:08:29 +0000

    aula 1

---
+++ src/main/java/br/com/yannngn/todolist/user/UserController.java
@@ -0,0 +1,29 @@
+package br.com.yannngn.todolist.user;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    /**
+     * String
+     * Integer
+     * Float
+     * Double
+     * char
+     * Date
+     * ...
+     * void
+     * 
+     */
+    /**
+     * Body
+     */
+    @PostMapping("/")
+    public void create(@RequestBody UserModel userModel) {
+        System.out.println(userModel.name);
+    }
+}
---
+++ src/main/java/br/com/yannngn/todolist/user/UserModel.java
@@ -0,0 +1,7 @@
+package br.com.yannngn.todolist.user;
+
+public class UserModel {
+    public String username;
+    public String name;
+    public String password;
+}
